9d5a44c15dd3b715c6a55cf61dd356e6712acc15
k.donahue45@yahoo.com <k.donahue45@yahoo.com>
2021-12-21 22:11:21 -0500

    Updated README index.html and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
-// api.openweathermap.org/data/2.5/weather?q={Phialdelphia}&appid={"66aac07673b20258f189cdd690356547"
-// }
+// GIVEN a weather dashboard with form inputs
+
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+
+// WHEN I view the UV index
+// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+
+// api.openweathermap.org/data/2.5/weather?q={Phialdelphia}&appid={"66aac07673b20258f189cdd690356547"}
 
 // api.openweathermap.org/data/2.5/forecast?q={Philadelphia}&appid={"66aac07673b20258f189cdd690356547"}
 
@@ -11,8 +27,6 @@
 // forecast.windSpeed.unit
 // forecast.humidity.unit
 
-
-
 // var mytable = "<table><tr>";
 // for (var CELL of ARRAY) {  mytable += "<td>" + CELL + "</td>"; }
 // mytable += "</tr></table>";
@@ -22,12 +36,13 @@
 // https://api.openweathermap.org/data/2.5/weather?q=philadelphia&appid=66aac07673b20258f189cdd690356547
 
 var apiKey = "66aac07673b20258f189cdd690356547";
-var cityName;
+var cityName; "#city";
 
 var searchBtn = document.querySelector("#search");
 var inputEl = document.querySelector("#input");
 var cityEl = document.querySelector("#city");
-var tempEl = document.querySelector("#temp")
+var humidity = document.querySelector("#humidity");
+var tempEl = document.querySelector("#temp");
 
 searchBtn.addEventListener("click", function(){
     cityName = inputEl.value;
@@ -51,6 +66,7 @@ searchBtn.addEventListener("click", function(){
             humidityEl.textContent = data.humidity;
             windSpeedEl.textContent = data.windSpeed;
             uvIndexEl.textContent = data.uv.index;
+
 
         })
 
